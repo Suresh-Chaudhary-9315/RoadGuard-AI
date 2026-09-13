deab76d6113342038a3923878b622658b0fa7cbd
Suresh Chaudhary <sureshchaudhary9315@gmail.com>
2026-09-13 10:32:46 +0530

    Use Railway port

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;;
 
   // Request parsers
   app.use(express.json({ limit: '50mb' }));
